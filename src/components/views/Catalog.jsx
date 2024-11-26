4e99989973bbc51181a7e9a3d2735f178c13a179
Kevin Perez <maur.av007@gmail.com>
2024-11-26 13:13:32 -0600

    Agregando view de editar productos

--- src/components/views/Catalog.jsx
+++ src/components/views/Catalog.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ModalEditProduct from "../ModalEditProduct";
 
 const Catalog = () => {
+
+  const [data, setData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   const apiUrl = "http://localhost/products/api.php";
   let config = {
@@ -13,8 +17,9 @@ const Catalog = () => {
   };
 
   const getAllProducts = async () => {
-    const res = axios.get(`${apiUrl}/products`, config);
+    const res = await axios.get(`${apiUrl}/productos`, config);
     console.log(res);
+    setData(res.data);
   }
 
   useEffect(() => {
@@ -24,94 +29,30 @@ const Catalog = () => {
   return (
     <section className="text-gray-400 bg-gray-900 body-font">
       <div className="p-4">
-          <Link to={"/registrar-producto"}>
+        <Link to={"/registrar-producto"}>
           <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Agregar producto</button>
-          </Link>
+        </Link>
       </div>
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">The Catalyzer</h2>
-              <p className="mt-1">$16.00</p>
+          {data.map((item, key) => {
+            return <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={key}>
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{item.id}</h3>
+                <h2 className="text-white title-font text-lg font-medium">{item.nombre}</h2>
+                <p className="mt-1">{item.precio}</p>
+              </div>
+              <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={() => setShowModal(true)}>Editar</button>
             </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">Shooting Stars</h2>
-              <p className="mt-1">$21.15</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">Neptune</h2>
-              <p className="mt-1">$12.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">The 400 Blows</h2>
-              <p className="mt-1">$18.40</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">The Catalyzer</h2>
-              <p className="mt-1">$16.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">Shooting Stars</h2>
-              <p className="mt-1">$21.15</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">Neptune</h2>
-              <p className="mt-1">$12.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-white title-font text-lg font-medium">The 400 Blows</h2>
-              <p className="mt-1">$18.40</p>
-            </div>
-          </div>
+          })
+          };
+
         </div>
       </div>
+        { showModal && <ModalEditProduct setShowModal={setShowModal} /> }
     </section>
   );
 };
--- src/components/views/RegisterProducto.jsx
+++ src/components/views/RegisterProducto.jsx
@@ -1,4 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const RegistrarProducto = () => {
+
+    const navigate = useNavigate();
+
+    const apiUrl = "http://localhost/products/api.php";
+    let config = {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    };
+
+    const dataProductInit = {
+        name: "",
+        description: "",
+        price: 0,
+    };
+
+    const [dataProduct, setDataProduct] = useState(dataProductInit);
+
+    const handleChange = (e) => {
+        //Actualizando el valor segun propiedad y valor que proviende del input
+        setDataProduct({
+            ...dataProduct,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(dataProduct);
+        console.log(dataProduct);
+        await axios.post(`${apiUrl}/productos`, dataProduct, config)
+            .then(response => {
+                console.log(response);
+                navigate("/catalog");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+    }
+
     return (
         <section className="text-gray-400 bg-gray-900 body-font relative">
             <div className="container px-5 py-24 mx-auto">
@@ -6,31 +51,31 @@ const RegistrarProducto = () => {
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Agregar producto</h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Ingresa a continuacion tus datos para agregar un producto</p>
                 </div>
-                <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                <form className="lg:w-1/2 md:w-2/3 mx-auto">
                     <div className="flex flex-wrap -m-2">
                         <div className="p-2 w-1/2">
                             <div className="relative">
-                                <label for="name" className="leading-7 text-sm text-gray-400">Nombre</label>
-                                <input type="text" id="name" name="name" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                <label htmlFor="name" className="leading-7 text-sm text-gray-400">Nombre</label>
+                                <input type="text" id="name" name="name" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleChange} placeholder="Ingresar el nombre del producto" required />
                             </div>
                         </div>
                         <div className="p-2 w-1/2">
                             <div className="relative">
-                                <label for="email" className="leading-7 text-sm text-gray-400">Precio</label>
-                                <input type="number" id="email" name="email" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                <label htmlFor="price" className="leading-7 text-sm text-gray-400">Precio</label>
+                                <input type="number" id="price" name="price" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleChange} placeholder="00.00" required />
                             </div>
                         </div>
                         <div className="p-2 w-full">
                             <div className="relative">
-                                <label for="message" className="leading-7 text-sm text-gray-400">Descripción</label>
-                                <textarea id="description" name="description" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                <label htmlFor="description" className="leading-7 text-sm text-gray-400">Descripción</label>
+                                <textarea id="description" name="description" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" onChange={handleChange} placeholder="Ingresar la descripción del producto" required />
                             </div>
                         </div>
                         <div className="p-2 w-full">
-                            <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Agregar</button>
+                            <button type="submit" className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={handleSubmit}>Agregar producto</button>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </section>
     );
